e4914e57c71fc8be31dbaca974753082590c17fc
Luis Samano <samanocedillo@outlook.com>
2018-12-05 23:02:09 -0600

    presentacion

--- open.py
+++ open.py
@@ -1,27 +1,3 @@
-"""
-# Abrir el entorno de Anaconda
-"C:\ProgramData\Anaconda3\Scripts\activate.bat"
-
-# Verificar las versiones de Python y pip
-python -V
-pip -V
-
-# Instalar pip
-python -m pip install -U pip
-
-# Actualizar pip
-python -m pip --upgrade -U pip
-
-# URL para descargar WHL de OpenCV
-http://www.lfd.uci.edu/~gohlke/pythonlibs/#opencv
-
-# Instalar el archivo pip
-pip install "opencv_python-3.4.4+contrib-cp37-cp37m-win32.whl"
-"""
-
-
-
-
 import cv2
  
 # Cargamos la imagen del disco duro
